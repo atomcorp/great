26436898893d1c1b974a044648cef226db10e7b2
atomcorp <thomasmaxwellsmith@gmail.com>
2022-03-06 22:46:58 +0000

    fix for iphone zoom?

--- src/styles/default-styles.ts
+++ src/styles/default-styles.ts
@@ -2,8 +2,11 @@ import {css} from 'lit';
 
 const defaultStyles = css`
   button,
+  button:focus,
   input,
-  textarea {
+  input:focus,
+  textarea,
+  textarea:focus {
     font-size: 18px;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
